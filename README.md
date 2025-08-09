5a0cc9ae1ccf14f6ed962e7ec85c571849d23bdb
Roihan <56998497+rroihans@users.noreply.github.com>
2025-08-09 18:24:33 +0700

    init project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Evenity
+
+A new Flutter project.
+
+Evenity adalah sebuah aplikasi android yang dikembangkan menggunakan Flutter berfungsi untuk melihat, filter, dan membuat events yang tergabung dalam satu database menggunakan API.
+
+Untuk memenuhi Ujian Akhir Semester Akhir TA 2024/2025 mata kuliah Pemrograman Bergerak. Dosen: Achmad Fachrie.
+
+Creator: Rizki Roihan Soeharto
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // format ID
+  await initializeDateFormatting('id_ID', null);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Evenity',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('init project'),
+        ),
+      ),
+    );
+  }
+}
